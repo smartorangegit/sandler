c99faeaf547ff25157b4e8aba55ad7b877289cf8
Andrew Basiuk <a.basyuk4779@gmail.com>
2018-07-06 14:35:48 +0300

    initial commit

---
+++ dist/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.min.css">
+    <title></title>
+</head>
+<body>
+    <?php/* include_once('includes/header.php');*/?>
+    
+    <section class="home__section">
+        <div class="page-slider-wrap">
+            
+        </div>
+    </section>
+
+    
+
+    <?php/* include_once('includes/footer.php');*/?> 
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/common.min.js"></script>
+<script src="js/home.min.js"></script>
+</body>
+</html>
---
+++ gulpfile.js
@@ -0,0 +1,138 @@
+var gulp           = require('gulp'),
+		gutil          = require('gulp-util' ),
+		sass           = require('gulp-sass'),
+		browserSync    = require('browser-sync'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
+		cleanCSS       = require('gulp-clean-css'),
+		rename         = require('gulp-rename'),
+		del            = require('del'),
+		imagemin       = require('gulp-imagemin'),
+		cache          = require('gulp-cache'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		ftp            = require('vinyl-ftp'),
+		notify         = require("gulp-notify");
+
+// Скрипты проекта
+
+// All start 
+gulp.task('all-js', function() {
+	return gulp.src([
+		'src/js/common.js',
+		])
+	.pipe(concat('all.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'));
+});
+// All end 
+
+// Common
+gulp.task('common-js', function() {
+	return gulp.src([
+		'src/js/libs/slick.min.js',
+		'src/js/common.js',
+		])
+	.pipe(concat('common.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'));
+});
+// Common
+
+// Home start
+gulp.task('home-js', function() {
+	return gulp.src([
+		'src/js/home.js',
+		])
+	.pipe(concat('home.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'));
+});
+// Home end
+
+gulp.task('js', [
+	'common-js',
+	'home-js',
+	], function() {
+	return gulp.src([
+		'src/js/common.js',
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify()) // Минимизировать весь js (на выбор)
+	.pipe(gulp.dest('dist/js'))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		proxy: "sandler/dist",
+    	notify: false // Отключаем уведомления
+	});
+});
+
+gulp.task('sass', function() {
+	return gulp.src(['src/sass/**/*.sass', 'src/sass/**/*.scss'])
+	.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleanCSS()) // Опционально, закомментировать при отладке
+	.pipe(gulp.dest('dist/css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', ['sass', 'js', 'browser-sync'], function() {
+	gulp.watch('src/sass/**/*.scss', ['sass']);
+	gulp.watch(['libs/**/*.js', 'src/**/*.js'], ['js']);
+	gulp.watch('dist/**/*.php', browserSync.reload);
+	gulp.watch('dist/*.html', browserSync.reload);
+});
+
+gulp.task('imagemin', function() {
+	return gulp.src('src/img/**/*')
+	.pipe(cache(imagemin()))
+	.pipe(gulp.dest('dist/img')); 
+});
+
+gulp.task('build', ['removedist', 'imagemin', 'sass', 'js'], function() {
+
+	var buildFiles = gulp.src([
+		'src/*.html',
+		'src/.htaccess',
+		]).pipe(gulp.dest('dist'));
+
+	var buildCss = gulp.src([
+		'src/css/main.min.css',
+		]).pipe(gulp.dest('dist/css'));
+
+	var buildJs = gulp.src([
+		'src/js/scripts.min.js',
+		]).pipe(gulp.dest('dist/js'));
+
+	var buildFonts = gulp.src([
+		'src/fonts/**/*',
+		]).pipe(gulp.dest('dist/fonts'));
+
+});
+
+/*gulp.task('deploy', function() {
+
+	var conn = ftp.create({
+		host:      'hostname.com',
+		user:      'username',
+		password:  'userpassword',
+		parallel:  10,
+		log: gutil.log
+	});
+
+	var globs = [
+	'dist/**',
+	'dist/.htaccess',
+	];
+	return gulp.src(globs, {buffer: false})
+	.pipe(conn.dest('/path/to/folder/on/server'));
+
+});*/
+
+gulp.task('removedist', function() { return del.sync('dist'); });
+gulp.task('clearcache', function () { return cache.clearAll(); });
+
+gulp.task('default', ['watch']);
